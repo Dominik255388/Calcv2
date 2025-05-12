a2d8c90f622a11636144b18aad7007d61bffd33d
Dominik255388 <255388@edu.p.lodz.pl>
2025-05-12 11:49:49 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -168,3 +168,64 @@ int main(int argc, char *argv[]) {
 }
 
 */
+/*
+#include <stdio.h>
+#include <stdlib.h>
+
+void usage(char *programname) {
+    printf("\nHow to use: %s <liczba1> <operator> <liczba2>\n", programname);
+    printf("Options: +, -, *, /\n");
+    exit(1);
+}
+
+int main(int argc, char *argv[]) {
+    printf("Ten program wykonuje proste działania matematyczne na dwóch liczbach.\n");
+    printf("Składnia: %s <liczba1> <operator> <liczba2>\n", argv[0]);
+    printf("Dostępne operatory: +, -, *, /\n\n");
+
+    int number1 = 0;
+    int number2 = 0;
+    int result = 0;
+
+    if (argc != 4) {
+        usage(argv[0]);  // Sprawdzenie liczby argumentów
+    }
+
+    number1 = atoi(argv[1]);  // Konwersja pierwszej liczby
+    number2 = atoi(argv[3]);  // Konwersja drugiej liczby
+    char op = argv[2][0];     // Operator jako pojedynczy znak
+
+    switch (op) {
+        case '+':
+            result = number1 + number2;
+            printf("%d + %d = %d\n", number1, number2, result);
+            break;
+
+        case '-':
+            result = number1 - number2;
+            printf("%d - %d = %d\n", number1, number2, result);
+            break;
+
+        case '*':
+            result = number1 * number2;
+            printf("%d * %d = %d\n", number1, number2, result);
+            break;
+
+        case '/':
+            if (number2 == 0) {
+                printf("Błąd: Dzielenie przez zero!\n");
+                return 1;
+            }
+            result = number1 / number2;
+            printf("%d / %d = %d\n", number1, number2, result);
+            break;
+
+        default:
+            printf("Nieznany operator: %s\n", argv[2]);
+            usage(argv[0]);
+    }
+
+    return 0;
+}
+
+*/
