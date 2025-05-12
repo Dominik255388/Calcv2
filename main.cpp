413287c2da7df133d2fa6d920f515c4d509dbcde
Dominik255388 <255388@edu.p.lodz.pl>
2025-05-12 13:15:51 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,119 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-
-void usage(char *programname) {
-printf("\nHow to use: %s <liczba1> <operator> <liczba2>\n", programname);
-printf("Options: add, sub, mul, div\n");
-exit(1);
-}
-
-int main(int argc, char *argv[]) {
-    int number1 = 0;
-    int number2 = 0;
-    int result = 0;
-    
-
-    if (argc != 4) {
-        usage(argv[0]);  //Sprawdzamy czy użyto właściwej liczby argumentów (w sesnie Licza1 i 2 oraz operator)
-    }
-
-    number1 = atoi(argv[1]); //Zamienia stringi na inty
-    number2 = atoi(argv[3]);
-
-    if (strcmp("add", argv[2]) == 0) {
-        result = number1 + number2;
-        printf("%d + %d = %d\n", number1, number2, result);
-    }
-    else if (strcmp("sub", argv[2]) == 0) {
-        result = number1 - number2;
-        printf("%d - %d = %d\n", number1, number2, result);
-    }
-    else if (strcmp("mul", argv[2]) == 0) {
-        result = number1 * number2;
-        printf("%d * %d = %d\n", number1, number2, result);
-    }
-    else if (strcmp("div", argv[2]) == 0) {
-        if (number2 == 0) {
-            printf("Błąd: Dzielenie przez zero!\n");
-            return 1;
-        }
-        result = number1 / number2;
-        printf("%d / %d = %d\n", number1, number2, result);
-    }
-    else {
-        printf("Nieznany operator: %s\n", argv[2]);
-        usage(argv[0]);
-    }
-
-    return 0;
-}
-
-/*
-#include <stdio.h>
-#include <stdlib.h>
-
-void usage(char *programname) {
-printf("\nHow to use: %s <liczba1> <operator> <liczba2>\n", programname);
-printf("Options: +, -, *, /\n");
-exit(1);
-}
-
-int main(int argc, char *argv[]) {
-    int number1 = 0;
-    int number2 = 0;
-    int result = 0;
-    
-
-    if (argc != 4) {
-        usage(argv[0]);  //Sprawdzamy czy użyto właściwej liczby argumentów (w sesnie Licza1 i 2 oraz operator)
-    }
-    
-    number1 = atoi(argv[1]); //Zamienia stringi na inty
-    number2 = atoi(argv[3]);
-    
-    char op = argv[2][0];
-
-    switch (op) {
-        
-        case '+':
-        result = number1 + number2;
-        printf("%d + %d = %d\n", number1, number2, result);
-        break;
-
-        case '-':
-        result = number1 - number2;
-        printf("%d - %d = %d\n", number1, number2, result);
-        break;
-
-        case '*':
-        result = number1 * number2;
-        printf("%d * %d = %d\n", number1, number2, result);
-        break;
-
-        case '/':
-            
-        if (number2 == 0) {
-            printf("Błąd: Dzielenie przez zero!\n");
-            return 1;
-        }
-            
-        result = number1 / number2;
-        printf("%d / %d = %d\n", number1, number2, result);
-        break;
-    
-        default:
-        printf("Nieznany operator: %s\n", argv[2]);
-        usage(argv[0]);
-
-    }
-    
-    return 0;
-}
-
-/*
-#include <stdio.h>
-#include <stdlib.h>
 
 void usage(char *programname) {
     printf("\nHow to use: %s <liczba1> <operator> <liczba2>\n", programname);
@@ -122,19 +8,21 @@ void usage(char *programname) {
 }
 
 int main(int argc, char *argv[]) {
+
     int number1 = 0;
     int number2 = 0;
     int result = 0;
 
     if (argc != 4) {
-        usage(argv[0]);  // Sprawdzamy, czy podano dokładnie 3 argumenty
+        usage(argv[0]);
     }
 
-    number1 = atoi(argv[1]);  // Konwersja stringów na int
+    number1 = atoi(argv[1]);
     number2 = atoi(argv[3]);
     char op = argv[2][0];
 
     switch (op) {
+
         case '+':
             result = number1 + number2;
             printf("%d + %d = %d\n", number1, number2, result);
@@ -166,66 +54,3 @@ int main(int argc, char *argv[]) {
 
     return 0;
 }
-
-*/
-/*
-#include <stdio.h>
-#include <stdlib.h>
-
-void usage(char *programname) {
-    printf("\nHow to use: %s <liczba1> <operator> <liczba2>\n", programname);
-    printf("Options: +, -, *, /\n");
-    exit(1);
-}
-
-int main(int argc, char *argv[]) {
-    printf("Ten program wykonuje proste działania matematyczne na dwóch liczbach.\n");
-    printf("Składnia: %s <liczba1> <operator> <liczba2>\n", argv[0]);
-    printf("Dostępne operatory: +, -, *, /\n\n");
-
-    int number1 = 0;
-    int number2 = 0;
-    int result = 0;
-
-    if (argc != 4) {
-        usage(argv[0]);  // Sprawdzenie liczby argumentów
-    }
-
-    number1 = atoi(argv[1]);  // Konwersja pierwszej liczby
-    number2 = atoi(argv[3]);  // Konwersja drugiej liczby
-    char op = argv[2][0];     // Operator jako pojedynczy znak
-
-    switch (op) {
-        case '+':
-            result = number1 + number2;
-            printf("%d + %d = %d\n", number1, number2, result);
-            break;
-
-        case '-':
-            result = number1 - number2;
-            printf("%d - %d = %d\n", number1, number2, result);
-            break;
-
-        case '*':
-            result = number1 * number2;
-            printf("%d * %d = %d\n", number1, number2, result);
-            break;
-
-        case '/':
-            if (number2 == 0) {
-                printf("Błąd: Dzielenie przez zero!\n");
-                return 1;
-            }
-            result = number1 / number2;
-            printf("%d / %d = %d\n", number1, number2, result);
-            break;
-
-        default:
-            printf("Nieznany operator: %s\n", argv[2]);
-            usage(argv[0]);
-    }
-
-    return 0;
-}
-
-*/
